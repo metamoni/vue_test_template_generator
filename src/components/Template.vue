03319b0f7cf3281fd8ef8bc327511d90af94afcf
Monica <monica@monicas-macbook.home>
2020-03-26 21:43:44 +0000

    add component for test template output

---
+++ src/components/Template.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h2>Your test template</h2>
+    <label for="generated-template">Template</label>
+    <textarea id="generated-template">Describe method name</textarea>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Template',
+
+};
+</script>
+
+<style scoped>
+label, textarea {
+  display: block;
+}
+
+textarea {
+  padding: 0.5em;
+  margin-bottom: 1.5em;
+  height: 50em;
+  width: 40em;
+}
+
+label {
+  margin-bottom: 0.5em;
+}
+
+@media screen and (max-width: 900px) {
+  textarea {
+    width: 100%;
+  }
+}
+</style>
