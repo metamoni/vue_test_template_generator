5520b744defc83a0eac17ad7e1cc9b2fbb039c15
Monica <monica@monicas-macbook.home>
2020-03-26 21:43:24 +0000

    add form for component details

---
+++ src/components/Form.vue
@@ -0,0 +1,52 @@
+<template>
+  <section>
+    <h2>Enter your component data</h2>
+    <label for="component-name">Component name</label>
+    <input id="component-name" />
+    <label for="method-name">Method name</label>
+    <input id="method-name" />
+    <label for="context-name">Context name</label>
+    <input id="context-name" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+section {
+  margin-right: 1em;
+}
+
+label, input {
+  display: block;
+}
+
+label {
+  margin-bottom: 0.5em;
+}
+
+input {
+  width: 40em;
+  padding: 0.5em;
+  margin-bottom: 1.5em;
+}
+
+@media screen and (max-width: 900px) {
+  input {
+    width: 70%;;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  input {
+    width: 100%;;
+  }
+}
+</style>
