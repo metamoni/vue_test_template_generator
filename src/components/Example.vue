ad8a9d14869a4a726cf04200a6c3247ebc6688a0
Monica <monica@monicas-macbook.home>
2020-04-12 21:04:06 +0100

    add Example component

---
+++ src/components/Example.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <article v-for="example in examples" :key="example">
+    <br/>
+      <span class="indent">
+          it('<span class="example">{{ example }}</span>', () => {</span>
+          <br/>
+          <span class="double-indent">// expect(...);</span>
+          <br/>
+          <span class="indent">});</span>
+    <br/>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Example',
+  data() {
+    return {
+      examples: this.$store.state.examples,
+    };
+  },
+};
+</script>
