f9355c32d0062a19441eeb3d1728c60f7e1f3a17
Monica <monica@monicas-macbook.home>
2020-04-12 21:03:41 +0100

    add example and context on button click
    - previously included a single example and context
    - converted example and context blocks to form

--- src/components/ComponentDetailForm.vue
+++ src/components/ComponentDetailForm.vue
@@ -1,29 +1,45 @@
 <template>
   <section>
     <h2>Enter your component data</h2>
-    <label for="component-name">Component name</label>
-    <input id="component-name" @keyup="updateName" :placeholder="this.$store.state.componentName" />
 
-    <label for="example">Test description</label>
-    <input id="example" @keyup="updateExample" :placeholder="this.$store.state.example" />
+    <label for="component-name">Component name</label><br/>
+    <input id="component-name" @keyup="updateName" :placeholder="name" />
 
-    <button>Add test</button>
-    <button @click="addContext">Add context</button>
+    <form id="example-form" @submit.prevent="submitExample()">
+      <label for="example">Test description</label><br/>
+      <input id="example" ref="exampleInput" :placeholder="example" />
+      <button type="submit">ADD</button>
+    </form>
+
+    <form id="context-form" @submit.prevent="submitContext()">
+      <label for="context">Context</label><br/>
+      <input id="context" ref="contextInput" :placeholder="context" />
+      <button type="submit">ADD</button>
+    </form>
   </section>
 </template>
 
 <script>
 export default {
   name: 'ComponentDetailForm',
+  data() {
+    return {
+      name: this.$store.state.name,
+      example: this.$store.state.example,
+      context: this.$store.state.context,
+    };
+  },
   methods: {
     updateName(event) {
       this.$store.commit('updateComponentName', event.target.value);
     },
-    updateExample(event) {
-      this.$store.commit('updateExample', event.target.value);
+    submitExample() {
+      this.$store.commit('addExample', this.$refs.exampleInput.value);
+      document.getElementById('example-form').reset();
     },
-    updateContext(event) {
-      this.$store.commit('updateContext', event.target.value);
+    submitContext() {
+      this.$store.commit('addContext', this.$refs.contextInput.value);
+      document.getElementById('context-form').reset();
     },
   },
 };
@@ -34,31 +50,61 @@ section {
   margin-right: 2em;
 }
 
-label, input {
-  display: block;
-}
-
-label {
-  margin-bottom: 0.5em;
-}
-
 input {
   color: #c7cfe2;
   font-size: 1em;
   width: 30em;
   padding: 0.5em;
   margin-bottom: 1.5em;
+  margin-top: 0.5em;
+}
+
+#example, #context {
+  width: 24em;
+}
+
+button {
+  width: 4em;
+  margin-left: 1em;
+  padding: 0.3em 0.7em;
+  font-size: 1.2em;
 }
 
 @media screen and (max-width: 1350px) {
   input {
-    width: 70%;;
+    width: 70%;
+  }
+
+  #example, #context {
+    width: 53%;
+  }
+
+  button {
+    width: 15%;
+  }
+}
+
+@media screen and (max-width:950px) {
+  input {
+    width: 93%;
+  }
+
+  #example, #context {
+    width: 76%;
+  }
+
+  button {
+    width: 15%;
   }
 }
 
 @media screen and (max-width: 700px) {
-  input {
-    width: 100%;;
+  #example, #context {
+    width: 68%;
+  }
+
+  button {
+    width: 20%;
   }
 }
 </style>
